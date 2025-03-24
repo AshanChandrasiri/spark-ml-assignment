47b9ea6a07979d29f919413e75405031547303ec
AshanChandrasiri <ashanchandrasiri1@gmail.com>
2025-03-25 03:32:54 +0530

    integrate prediction results with API

--- main.py
+++ main.py
@@ -19,26 +19,27 @@ import nltk
 from nltk.stem import PorterStemmer
 from pyspark.ml.classification import LogisticRegressionModel
 
-# from pyngrok import ngrok
+from pyngrok import ngrok
 import threading
 
 app = Flask(__name__)
 
-# model_base_path = "./saved_model"
-# spark = (SparkSession.builder.appName("MusicClassification-app")
-#          .config("spark.hadoop.io.native.lib", "false")
-#          .getOrCreate())
-#
-# lr_model_path = os.path.join(model_base_path, "logistic_regression-v2")
-# lr_model_path = os.path.abspath(lr_model_path)
-#
-# lr_model = LogisticRegressionModel.load(lr_model_path)
-# word2Vec_model = Word2VecModel.load(os.path.join(model_base_path, 'word2vec-new'))
-#
-# tokenizer = Tokenizer(inputCol="clean_lyrics", outputCol="words")
-# stop_words_remover = StopWordsRemover(inputCol="words", outputCol="filtered_words")
-# nltk.download("punkt")
-# stemmer = PorterStemmer()
+model_base_path = "./saved_model"
+spark = (SparkSession.builder.appName("MusicClassification-app")
+         .config("spark.hadoop.io.native.lib", "false")
+         .getOrCreate())
+
+lr_model_path = os.path.join(model_base_path, "logistic_regression-v2")
+lr_model_path = os.path.abspath(lr_model_path)
+
+print(lr_model_path)
+lr_model = LogisticRegressionModel.load(lr_model_path)
+word2Vec_model = Word2VecModel.load(os.path.join(model_base_path, 'word2vec-new'))
+
+tokenizer = Tokenizer(inputCol="clean_lyrics", outputCol="words")
+stop_words_remover = StopWordsRemover(inputCol="words", outputCol="filtered_words")
+nltk.download("punkt")
+stemmer = PorterStemmer()
 
 label_map = {
     0: 'pop',
@@ -69,37 +70,37 @@ def predict():
         if not model or not lyrics:
             return jsonify({"error": "Missing model or lyrics"}), 400
 
-        # model = lr_model
-        #
-        # df = spark.createDataFrame([(lyrics,)], ["lyrics"])
-        # df = df.withColumn("clean_lyrics", lower(col("lyrics")))
-        # df = df.withColumn("clean_lyrics", regexp_replace(col("clean_lyrics"), "[^a-zA-Z\\s]", ""))
-        #
-        # df = tokenizer.transform(df)
-        #
-        # df = stop_words_remover.transform(df)
-        #
-        # stem_udf = udf(lambda words: [stemmer.stem(word) for word in words], ArrayType(StringType()))
-        # df = df.withColumn("stemmed_words", stem_udf(col("filtered_words")))
-        #
-        # df = word2Vec_model.transform(df)
-        #
-        # y_pred = model.transform(df)
-        # predicted_genre = label_map[int(y_pred.collect()[0]['prediction'])]
-        # prediction_probs = y_pred.collect()[0]["probability"][int(y_pred.collect()[0]['prediction'])]
+        model = lr_model
 
-        pred_results = {
-            'pop': 0.65,
-            'country': 0.15,
-            'blues': 0.1,
-            'rock': 0.01,
-            'jazz': 0.02,
-            'reggae': 0.05,
-            'hip hop': 0.02
-        }
+        df = spark.createDataFrame([(lyrics,)], ["lyrics"])
+        df = df.withColumn("clean_lyrics", lower(col("lyrics")))
+        df = df.withColumn("clean_lyrics", regexp_replace(col("clean_lyrics"), "[^a-zA-Z\\s]", ""))
 
-        # probabilities = y_pred.collect()[0]['probability'].toArray()
-        # pred_results = {label_map[i]: float(probabilities[i]) for i in range(len(probabilities))}
+        df = tokenizer.transform(df)
+
+        df = stop_words_remover.transform(df)
+
+        stem_udf = udf(lambda words: [stemmer.stem(word) for word in words], ArrayType(StringType()))
+        df = df.withColumn("stemmed_words", stem_udf(col("filtered_words")))
+
+        df = word2Vec_model.transform(df)
+
+        y_pred = model.transform(df)
+        predicted_genre = label_map[int(y_pred.collect()[0]['prediction'])]
+        prediction_probs = y_pred.collect()[0]["probability"][int(y_pred.collect()[0]['prediction'])]
+
+        # pred_results = {
+        #     'pop': 0.65,
+        #     'country': 0.15,
+        #     'blues': 0.1,
+        #     'rock': 0.01,
+        #     'jazz': 0.02,
+        #     'reggae': 0.05,
+        #     'hip hop': 0.02
+        # }
+
+        probabilities = y_pred.collect()[0]['probability'].toArray()
+        pred_results = {label_map[i]: float(probabilities[i]) for i in range(len(probabilities))}
 
         labels = list(pred_results.keys())
         sizes = list(pred_results.values())
@@ -127,23 +128,20 @@ def predict():
         bar_image_base64 = base64.b64encode(buffer.getvalue()).decode()
 
         resp = {
-            "prediction_label": "POP",
-                "prediction_score": "0.58",
+            "prediction_label": predicted_genre,
+            "prediction_score": prediction_probs,
             "bar_chart": f"data:image/png;base64,{bar_image_base64}",
             "pie_chart": f"data:image/png;base64,{pie_image_base64}"
         }
 
         return jsonify(resp)
-
     except Exception as e:
         print(traceback.format_exc())
         raise e
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-    # public_url = ngrok.connect(port).public_url
-    # print(f" * ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{port}\"")
-    # app.config["BASE_URL"] = public_url
-    # threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
+    public_url = ngrok.connect(port).public_url
+    print(f" * ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{port}\"")
+    app.config["BASE_URL"] = public_url
+    threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
