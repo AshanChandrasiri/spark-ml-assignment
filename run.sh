24126b5cc9ef24cdb4a01900e8d07b6384daf5b6
AshanChandrasiri <ashanchandrasiri1@gmail.com>
2025-03-27 22:45:12 +0530

    add fixes

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
-#pip install -r ./requirements.txt
-
-#ngrok config add-authtoken 1S01nCUmkP05WRMTu7uQeG6STDq_2jAftaKdrwSB8QsSMKpj9
+pip install -r ./requirements.txt
 
 python main.py
+
+# Create Virtual environment
+python -m venv venv
+
+# Activate the virtual environment
+source venv/Scripts/activate
+
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install -r ./requirements.txt
+
+echo "Running main.py..."
+python ./main.py
